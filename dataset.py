6687253cd7ca6647cb9c9830e96a86757b3847c4
aakashvardhan <vardhan.aakash1@gmail.com>
2024-06-04 11:27:40 +0400

    refactor: Remove unnecessary code and update configuration parameters

--- dataset.py
+++ dataset.py
@@ -11,9 +11,69 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
-from train import get_all_sentenses, get_or_build_tokenizer, get_ds
+def get_all_sentenses(ds, lang):
+    for item in ds:
+        yield item["translation"][lang]
 
 
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config["tokenizer_file"].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[SOS]", "[EOS]", "[PAD]"], min_frequency=2
+        )
+        tokenizer.train_from_iterator(get_all_sentenses(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
+
+def get_ds(config):
+
+    ds_raw = load_dataset(
+        "opus_books", f"{config['lang_src']}-{config['lang_tgt']}", split="train"
+    )
+
+    src_lang = config["lang_src"]
+    tgt_lang = config["lang_tgt"]
+    seq_len = config["seq_len"]
+
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, src_lang)
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, tgt_lang)
+
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BillingualDataset(
+        train_ds_raw, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len
+    )
+    val_ds = BillingualDataset(
+        val_ds_raw, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len
+    )
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item["translation"][src_lang]).ids
+        tgt_ids = tokenizer_tgt.encode(item["translation"][tgt_lang]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f"Max length of the source sentence : {max_len_src}")
+    print(f"Max length of the source target : {max_len_tgt}")
+
+    train_dataloader = DataLoader(
+        train_ds, batch_size=config["batch_size"], shuffle=True
+    )
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 class BillingualDataset(Dataset):
     """
     A PyTorch dataset for handling bilingual text data.
