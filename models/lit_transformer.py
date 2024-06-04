7dec27eae8596db72608dfc8ea866aec3389c51a
aakashvardhan <vardhan.aakash1@gmail.com>
2024-06-04 11:34:49 +0400

    refactor: Remove unnecessary code and update configuration parameters

--- models/lit_transformer.py
+++ models/lit_transformer.py
@@ -6,8 +6,46 @@ import torch.nn as nn
 import torchmetrics
 from transformer import ProjectionLayer, Transformer, build_transformer
 
-from train import greedy_decode, get_model
 
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len):
+
+    sos_idx = tokenizer_tgt.token_to_id("[SOS]")
+    eos_idx = tokenizer_tgt.token_to_id("[EOS]")
+
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with SOS token
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+        decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask)
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+        prob = model.project(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat(
+            [
+                decoder_input,
+                torch.empty(1, 1).type_as(source_mask).fill_(next_word.item()),
+            ],
+            dim=1,
+        )
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
+
+
+def get_model(config, src_vocab_size, tgt_vocab_size):
+    model = build_transformer(
+        src_vocab_size,
+        tgt_vocab_size,
+        config["seq_len"],
+        config["seq_len"],
+        d_model=config["d_model"],
+    )
+    return model
 
 class LT_model(L.LightningModule):
     """
--- train.py
+++ train.py
@@ -35,45 +35,6 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:12240"
 config = get_config()
 
 
-def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len):
-
-    sos_idx = tokenizer_tgt.token_to_id("[SOS]")
-    eos_idx = tokenizer_tgt.token_to_id("[EOS]")
-
-    encoder_output = model.encode(source, source_mask)
-    # Initialize the decoder input with SOS token
-    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source)
-    while True:
-        if decoder_input.size(1) == max_len:
-            break
-        decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask)
-        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
-
-        prob = model.project(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        decoder_input = torch.cat(
-            [
-                decoder_input,
-                torch.empty(1, 1).type_as(source_mask).fill_(next_word.item()),
-            ],
-            dim=1,
-        )
-
-        if next_word == eos_idx:
-            break
-
-    return decoder_input.squeeze(0)
-
-
-def get_model(config, src_vocab_size, tgt_vocab_size):
-    model = build_transformer(
-        src_vocab_size,
-        tgt_vocab_size,
-        config["seq_len"],
-        config["seq_len"],
-        d_model=config["d_model"],
-    )
-    return model
 
 
 def main(cfg, ckpt_file, if_ckpt=False):
